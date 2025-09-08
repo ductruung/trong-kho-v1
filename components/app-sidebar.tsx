910a601aa89fa3a6eff8bd1ca52b68e6dc7f916c
Trung Le <dctrung0108@gmail.com>
2025-09-08 23:31:43 +0700

    Enhance AppSidebar with active state and routing
    Added 'use client' directive and integrated usePathname from next/navigation to highlight the active sidebar menu item based on the current route. Updated menu item URLs to use proper paths and improved icon sizing for better UI consistency.

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -1,5 +1,5 @@
-import { Shapes, Warehouse, Zap } from "lucide-react"
-
+"use client"
+import { Shapes, Warehouse, Zap } from "lucide-react";
 import {
   Sidebar,
   SidebarContent,
@@ -10,27 +10,30 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import { usePathname } from "next/navigation";
 
 // Menu items.
 const items = [
   {
     title: "Tổng quan",
-    url: "",
+    url: "/",
     icon: <Shapes size={20}/>,
   },
   {
     title: "Hoạt động",
-    url: "#",
+    url: "/hoat-dong",
     icon: <Zap size={20}/>,
   },
   {
     title: "Kho",
-    url: "#",
+    url: "/kho",
     icon: <Warehouse size={20}/>,
   },
 ]
 
 export function AppSidebar() {
+  const pathname = usePathname();
+  
   return (
     <Sidebar className="w-12 hover:w-40 duration-80 py-12 bg-background">
       <SidebarContent className="bg-background">
@@ -38,8 +41,8 @@ export function AppSidebar() {
           <SidebarGroupContent>
             <SidebarMenu>
               {items.map((item) => (
-                <SidebarMenuItem className="text-muted-foreground font-medium" key={item.title}>
-                  <SidebarMenuButton asChild>
+                <SidebarMenuItem className="text-muted-foreground font-medium [&>svg]:size-8" key={item.title}>
+                  <SidebarMenuButton isActive={pathname === item.url} asChild>
                     <a href={item.url}>
                       {item.icon}
                       <span>{item.title}</span>
