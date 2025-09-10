54b7f05628a623ba16e872e140aefceb7d05c33b
Trung Le <dctrung0108@gmail.com>
2025-09-11 02:23:39 +0700

    Make sortable the sorting dropdown menu

--- components/hoat-dong/SortDropdownMenu.tsx
+++ components/hoat-dong/SortDropdownMenu.tsx
@@ -1,76 +1,52 @@
 import { useState } from "react"
-import { ArrowUpDown, Plus, X } from "lucide-react"
+import { ArrowUpDown, ChevronDown, Plus, X } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
-import { Input } from "@/components/ui/input"
-import { FilterCalendar } from "./FilterCalendar"
 import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu"
+import {
+  DndContext,
+  closestCenter,
+  KeyboardSensor,
+  PointerSensor,
+  useSensor,
+  useSensors,
+  DragEndEvent,
+} from '@dnd-kit/core'
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable'
+import SortItemRow from "./SortItemRow"
+import { SortItem } from "./types"
 
 export function SortDropdownMenu() {
-  const [sortItems, setSortItems] = useState([
-    // { id: "1", column: "id", comparator: undefined, value: undefined },
-    // { id: "2", column: "date", comparator: undefined, value: undefined },
-    // { id: "3", column: "time", comparator: undefined, value: undefined },
-    // { id: "4", column: "type", comparator: undefined, value: undefined },
+  const [sortItems, setSortItems] = useState<SortItem[]>([
+    { id: "a", column: "id", type: "asc" },
+    { id: "b", column: "date", type: "asc" },
+    { id: "c", column: "time", type: "asc" },
+    { id: "d", column: "type", type: "asc" },
   ]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    })
+  );
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+    if (active.id !== over!.id) {
+      setSortItems((items) => {
+        console.log("active: ", active);
+        console.log("over: ", over);
+        const oldIndex = items.findIndex(item => item.id === active.id);
+        const newIndex = items.findIndex(item => item.id === over!.id);
 
-  const SortItem = ({ item, key }) => (
-    <div className="grid gap-2 grid-cols-[minmax(0,128px)_minmax(0,60px)_minmax(0,143px)_minmax(0,26px)]">
-      <Select value={item.key}>
-        <SelectTrigger size="sm" className="dark:bg-popover dark:hover:bg-popover dark:hover:border-muted-foreground/50 text-xs w-full">
-          <SelectValue />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem className="text-xs" value="id">Mã</SelectItem>
-          <SelectItem className="text-xs" value="date">Ngày</SelectItem>
-          <SelectItem className="text-xs" value="time">Giờ</SelectItem>
-          <SelectItem className="text-xs" value="type">Loại</SelectItem>
-        </SelectContent>
-      </Select>
-      {(item.key === "date" || item.key === "time") &&
-        <Select>
-          <SelectTrigger size="sm" className="dark:bg-popover dark:hover:bg-popover dark:hover:border-muted-foreground/50 text-xs w-full">
-            <SelectValue />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem className="text-xs" value="equal">=</SelectItem>
-            <SelectItem className="text-xs" value="after">&gt;</SelectItem>
-            <SelectItem className="text-xs" value="before">&lt;</SelectItem>
-          </SelectContent>
-        </Select>}
-      {item.key === "id" &&
-        <Input
-          className="col-start-3 focus-visible:ring-0 text-xs md:text-xs transition duration-200 dark:bg-popover h-8"
-          placeholder="Mã"
-        />
-      }
-      {item.key === "date" &&
-        <FilterCalendar item={{ id: "tmp", key: "date", comparator: "none", value: undefined }} onValueChange={() => {}} className="col-start-3" />
-      }
-      {item.key === "time" &&
-        <Input
-          placeholder="HH:MM"
-          pattern="[0-9]{2}:[0-9]{2}"
-          className="text-xs md:text-xs dark:bg-popover appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none focus-visible:ring-0 transition duration-200 h-8"
-        />
-      }
-      {item.key === "type" &&
-        <Select>
-          <SelectTrigger size="sm" className="col-start-3 dark:bg-popover dark:hover:bg-popover dark:hover:border-muted-foreground/50 w-full text-xs">
-            <SelectValue />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem className="text-xs" value="checkin">Nhập kho</SelectItem>
-            <SelectItem className="text-xs" value="checkout">Xuất kho</SelectItem>
-          </SelectContent>
-        </Select>
-      }
-      <Button size="icon" variant="ghost" className="text-muted-foreground cursor-pointer">
-        <X className="size-[14px]" />
-      </Button>
-    </div>
-  )
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
 
   return (
     <DropdownMenu>
@@ -82,43 +58,60 @@ export function SortDropdownMenu() {
       </DropdownMenuTrigger>
       <DropdownMenuContent
         align="start"
-        className="w-96"
+        className="w-96 p-0"
       >
-        <div className="p-2">
+        <div>
           {
             sortItems.length === 0 ?
-              <div>
+              <div className="p-2">
                 <p className="text-sm font-semibold text-muted-foreground mb-1.5">Chưa có cách sắp xếp nào được áp dụng</p>
                 <p className="text-xs text-muted-foreground">Thêm một tiêu chí sắp xếp phía dưới để sắp xếp dữ liệu hoạt động</p>
               </div>
               :
-              <div className="flex flex-col gap-1">
-                {sortItems.map((item, _) => { return <SortItem key={_} item={item} /> })}
+              <div className="space-y-2 py-2">
+                <DndContext
+                  sensors={sensors}
+                  collisionDetection={closestCenter}
+                  onDragEnd={handleDragEnd}
+                  autoScroll={false}
+                >
+                  <SortableContext
+                    items={sortItems}
+                    strategy={verticalListSortingStrategy}
+                  >
+                    {sortItems.map((item, _) => <SortItemRow key={item.id} itemIndex={sortItems.findIndex(c => item.id === c.id)} item={item} />)}
+                  </SortableContext>
+                </DndContext>
               </div>
           }
         </div>
-        <DropdownMenuSeparator />
-        <div className="flex justify-between items-center py-2 px-2">
+        <DropdownMenuSeparator className="mx-0 my-0" />
+        <div className="flex justify-between items-center py-3 px-3">
           <DropdownMenu>
             <DropdownMenuTrigger
               asChild
             >
               <Button className="text-xs h-fit py-1 rounded-sm cursor-pointer" variant="ghost" size="sm">
-                <Plus className="text-muted-foreground size-[14px]" />
                 Thêm tiêu chí sắp xếp
+                <ChevronDown className="text-muted-foreground size-[14px]" />
               </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent
               align="start"
-              className="flex flex-col p-1 text-xs"
+              className="flex flex-col p-1 text-xs w-32"
             >
-              <DropdownMenuItem className="p-1.5 pl-2 hover">Mã</DropdownMenuItem>
-              <DropdownMenuItem className="p-1.5 pl-2 hover">Ngày</DropdownMenuItem>
-              <DropdownMenuItem className="p-1.5 pl-2 hover">Giờ</DropdownMenuItem>
-              <DropdownMenuItem className="p-1.5 pl-2 hover">Loại</DropdownMenuItem>
+              <DropdownMenuItem className="p-1.5 pl-2 hover:bg-accent rounded-sm">Mã</DropdownMenuItem>
+              <DropdownMenuItem className="p-1.5 pl-2 hover:bg-accent rounded-sm">Ngày</DropdownMenuItem>
+              <DropdownMenuItem className="p-1.5 pl-2 hover:bg-accent rounded-sm">Giờ</DropdownMenuItem>
+              <DropdownMenuItem className="p-1.5 pl-2 hover:bg-accent rounded-sm">Loại</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
-          <Button className="text-xs h-fit py-1 rounded-sm cursor-pointer" variant="outline" size="sm">
+          <Button
+            className="text-xs h-fit py-1 rounded-sm cursor-pointer"
+            variant="outline"
+            size="sm"
+            disabled={sortItems.length === 0}
+          >
             Áp dụng cách sắp xếp
           </Button>
         </div>
---
+++ components/hoat-dong/SortItemRow.tsx
@@ -0,0 +1,63 @@
+import { ArrowDownNarrowWide, ArrowDownWideNarrow, Menu, X } from "lucide-react"
+import { Button } from "../ui/button"
+import { ToggleGroup, ToggleGroupItem } from "../ui/toggle-group"
+import {CSS} from '@dnd-kit/utilities'
+import { cn } from "@/lib/utils"
+import {useSortable} from '@dnd-kit/sortable'
+
+export default function SortItemRow({ item, itemIndex }) {
+  const {attributes, listeners, setNodeRef, transform, transition} = useSortable({
+    id: item.id,
+    transition: {
+      duration: 150, 
+      easing: 'cubic-bezier(0.25, 1, 0.5, 1)',
+    },
+  });
+  const sortableStyle = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div 
+      ref={setNodeRef} 
+      className={cn("flex gap-3 px-3 items-center")}
+      style={sortableStyle}
+      {...attributes}
+    >
+      <Menu {...listeners} className="size-[16px] hover:text-foreground transition duration-200 text-muted-foreground cursor-pointer" />
+      <div className="grow">
+        <span className="text-xs text-muted-foreground">
+          {itemIndex !== 0 ? "rồi theo" : "sắp xếp theo"}
+          <span className="text-sm text-foreground">
+            {item.column === "id" && " Mã"}
+            {item.column === "date" && " Ngày"}
+            {item.column === "time" && " Giờ"}
+            {item.column === "type" && " Loại"}
+          </span>
+        </span>
+      </div>
+      
+      <ToggleGroup
+        size="sm"
+        variant="outline"
+        type="single"
+      >
+        <ToggleGroupItem className="text-muted-foreground data-[state=on]:text-trongkho-foreground cursor-pointer data-[state=on]:border-border p-1 h-fit" value="one" aria-label="Tăng dần">
+          <ArrowDownNarrowWide className="size-[14px]" />
+        </ToggleGroupItem>
+        <ToggleGroupItem className="text-muted-foreground data-[state=on]:text-trongkho-foreground cursor-pointer p-1 h-fit" value="two" aria-label="Giảm dần">
+          <ArrowDownWideNarrow className="size-[14px]" />
+        </ToggleGroupItem>
+      </ToggleGroup>
+      <span className="text-xs text-muted-foreground">tăng dần</span>
+      <Button
+        size="icon"
+        variant="ghost"
+        className="text-muted-foreground cursor-pointer h-fit p-2 w-fit"
+      >
+        <X className="size-[14px]" />
+      </Button>
+    </div>
+  );
+}
