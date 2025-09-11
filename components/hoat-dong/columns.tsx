743e36d0836009fcc7161fd5b37cc1efea2d86a9
Trung Le <dctrung0108@gmail.com>
2025-09-11 21:30:39 +0700

    Add sort dropdown for table headers

--- components/hoat-dong/columns.tsx
+++ components/hoat-dong/columns.tsx
@@ -2,30 +2,87 @@
 
 import { ColumnDef } from "@tanstack/react-table"
 import { Activity } from "./types"
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
+import { Button } from "../ui/button"
+import { ArrowDown, ArrowUp, ChevronDown } from "lucide-react"
+
+function SortDropdown() {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant="ghost"
+          className="p-0 h-fit w-fit has-[>svg]:px-1 py-1 cursor-pointer rounded-sm"
+        >
+          <ChevronDown className="size-[14px] text-muted-foreground" />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent
+        align="end"
+        sideOffset={8}
+        alignOffset={-8}
+        className="rounded-sm"
+      >
+        <DropdownMenuItem className="text-xs text-muted-foreground">
+          <ArrowUp />
+          Sắp xếp tăng dần
+        </DropdownMenuItem>
+        <DropdownMenuItem className="text-xs text-muted-foreground">
+          <ArrowDown />
+          Sắp xếp giảm dần
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
 
 export const columns: ColumnDef<Activity>[] = [
   {
     accessorKey: "id",
-    header: "Mã",
+    header: () =>
+      <div className="w-full flex items-center justify-between">
+        <span>Mã</span>
+        <SortDropdown />
+      </div>,
   },
   {
     accessorKey: "created_date",
-    header: "Ngày",
+    header: () =>
+      <div className="w-full flex items-center justify-between">
+        <span>Ngày</span>
+        <SortDropdown />
+      </div>,
   },
   {
     accessorKey: "created_time",
-    header: "Giờ",
+    header: () =>
+      <div className="w-full flex items-center justify-between">
+        <span>Giờ</span>
+        <SortDropdown />
+      </div>,
   },
   {
     accessorKey: "product",
-    header: "Sản phẩm"
+    header: () =>
+      <div className="w-full flex items-center justify-between">
+        <span>Sản phẩm</span>
+        <SortDropdown />
+      </div>
   },
   {
     accessorKey: "type",
-    header: "Loại"
+    header: () =>
+      <div className="w-full flex items-center justify-between">
+        <span>Loại</span>
+        <SortDropdown />
+      </div>
   },
   {
     accessorKey: "by",
-    header: ""
+    header: () =>
+      <div className="w-full flex items-center justify-between">
+        <span></span>
+        <SortDropdown />
+      </div>
   }
 ]
--- components/hoat-dong/data-table.tsx
+++ components/hoat-dong/data-table.tsx
@@ -14,6 +14,7 @@ import {
   TableHead,
   TableHeader,
   TableRow,
+  TableFooter
 } from "@/components/ui/table"
 
 interface DataTableProps<TData, TValue> {
@@ -32,49 +33,54 @@ export function DataTable<TData, TValue>({
   })
 
   return (
-    <div className="overflow-hidden rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
-                return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
-                          header.column.columnDef.header,
-                          header.getContext()
-                        )}
-                  </TableHead>
-                )
-              })}
+    <Table>
+      <TableHeader>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <TableRow key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              return (
+                <TableHead className="border-r bg-card text-xs font-bold" key={header.id}>
+                  {header.isPlaceholder
+                    ? null
+                    : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext()
+                      )}
+                </TableHead>
+              )
+            })}
+          </TableRow>
+        ))}
+      </TableHeader>
+      <TableBody>
+        {table.getRowModel().rows?.length ? (
+          table.getRowModel().rows.map((row) => (
+            <TableRow
+              key={row.id}
+              data-state={row.getIsSelected() && "selected"}
+              
+            >
+              {row.getVisibleCells().map((cell) => (
+                <TableCell 
+                  key={cell.id}
+                  className="border-r text-xs"
+                >
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </TableCell>
+              ))}
             </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
-              </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
-    </div>
+          ))
+        ) : (
+          <TableRow>
+            <TableCell colSpan={columns.length} className="h-24 text-center">
+              No results.
+            </TableCell>
+          </TableRow>
+        )}
+      </TableBody>
+      <TableFooter>
+
+      </TableFooter>
+    </Table>
   )
 }
