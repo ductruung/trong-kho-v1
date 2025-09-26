cbb0a1f01a7b778985150e177b27ad0097ded2ea
Trung Le <dctrung0108@gmail.com>
2025-09-26 21:17:16 +0700

    Add download button to toolbar

--- components/hoat-dong/ToolBar.tsx
+++ components/hoat-dong/ToolBar.tsx
@@ -1,4 +1,4 @@
-import { Plus, RotateCcw } from "lucide-react"
+import { Download, Plus, RotateCcw } from "lucide-react"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import { FilterDropdownMenu } from "./FilterDropdownMenu"
@@ -9,10 +9,14 @@ export function ToolBar() {
     <div className="w-auto flex items-center gap-3 border-r h-10 bg-card px-3 py-3">
       <Button className="text-xs h-fit py-1 rounded-sm cursor-pointer" variant="ghost" size="sm">
         <RotateCcw className="text-muted-foreground size-[14px]" />
-        Tải lại bảng
+        Cập nhật
       </Button>
       <FilterDropdownMenu />
       <SortDropdownMenu />
+      <Button className="text-xs h-fit py-1 rounded-sm cursor-pointer"  variant="ghost" size="sm">
+        <Download className="text-muted-foreground size-[14px]" />
+        Tải bảng
+      </Button>
       <Separator orientation="vertical" />
       <Button
         className="bg-trongkho-foreground/40 border border-bg-trongkho-foreground/60 text-xs duration-200 text-white hover:bg-trongkho-foreground/50 hover:border-bg-trongkho-foreground/70 h-fit py-1 rounded-sm ml-3 cursor-pointer"
